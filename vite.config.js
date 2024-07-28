2befbc4023e540dd707889fd440c474e7cfeac75
Viji <vijilearn123@gmail.com>
2024-07-28 18:39:45 +0530

    corrections done both files

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 })
 
-export default {
-  build: {
-    outDir: 'dist'
-  }
-}
